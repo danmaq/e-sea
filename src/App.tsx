974dfbbfd2c4b06b919cabae4590f6cac95a4486
danmaq <info@danmaq.com>
2019-05-24 18:14:21 +0900

    Linked the added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { Link, Router } from '@reach/router';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Router } from '@reach/router';
 import React from 'react';
-import { Root, Routes, addPrefetchExcludes } from 'react-static';
-import FancyDiv from '~/components/FancyDiv';
-import Dynamic from '~/containers/Dynamic';
-import './app.css';
-
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic']);
+import { Root, Routes } from 'react-static';
 
 const FC: React.FC = () => (
   <Root>
-    <React.StrictMode>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
-      <div className="content">
-        <FancyDiv>
-          <React.Suspense fallback={<CircularProgress />}>
-            <Router>
-              <Dynamic path="dynamic" />
-              <Routes path="*" />
-            </Router>
-          </React.Suspense>
-        </FancyDiv>
-      </div>
-    </React.StrictMode>
+    <CssBaseline />
+    <React.Suspense fallback={<CircularProgress />}>
+      <Router>
+        <Routes path="*" />
+      </Router>
+    </React.Suspense>
   </Root>
 );
 FC.displayName = 'App';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
+import Copy from '~/atoms/Copy';
+import Hero from '~/atoms/Hero';
 
-const FC: React.FC = () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static + TypeScript</h1>
-    <p>
-      Learn
-      <a href="https://github.com/sw-yx/react-typescript-cheatsheet">
-        React + TypeScript
-      </a>
-    </p>
-    <p>
-      <a href="https://twitter.com/swyx">Report issues with this template</a>
-    </p>
-  </div>
-);
+const FC: React.FC = () => {
+  return (
+    <main>
+      <Hero />
+      <Copy>Take it, Easy.</Copy>
+    </main>
+  );
+};
 FC.displayName = 'Pages';
 
 export default FC;
