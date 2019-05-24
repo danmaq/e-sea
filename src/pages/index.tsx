999d334c53e2af2b0f391a009b134a182491aaf0
danmaq <info@danmaq.com>
2019-05-24 23:18:08 +0900

    Implemented the top page contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Copy from '~/atoms/Copy';
 import Hero from '~/atoms/Hero';
+import ParallaxImage from '~/atoms/ParallaxImage';
 
 const FC: React.FC = () => {
   return (
     <main>
       <Hero />
+      <Copy>Take it, Easy.</Copy>
+      <ParallaxImage />
       <Copy>Take it, Easy.</Copy>
     </main>
   );
