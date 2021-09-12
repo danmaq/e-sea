1a44bc1aaa8cba67199fd4b50ef225b80f898062
danmaq <info@danmaq.com>
2021-09-12 14:27:11 +0900

    feat: improved the metadata

--- src/atoms/Helmet.tsx
+++ src/atoms/Helmet.tsx
@@ -6,43 +6,40 @@ import { meta as metaData } from '~/intl/messages';
 
 const intl = new IntlMessage();
 
-const FC: React.FC = () => (
-  <Head>
-    <title>{intl.format(metaData.title.body)}</title>
-    <meta
-      property="og:site_name"
-      content={intl.format(metaData.title.body) as string}
-    />
-    <meta
-      property="og:title"
-      content={intl.format(metaData.title.body) as string}
-    />
-    <meta
-      name="twitter:title"
-      content={intl.format(metaData.title.body) as string}
-    />
+const FC: React.FC = () => {
+  const description = intl.format(metaData.description.body) as string;
+  const title = intl.format(metaData.title.body) as string;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="coverage" content="Worldwide" />
+      <meta name="description" content={description} />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="msapplication-tooltip" content={description} />
+      <meta name="rating" content="General" />
+      <meta name="referer" content="same-origin" />
+      <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
 
-    <meta property="og:url" content="/images/logo.png" />
-    <meta property="og:image" content="/images/logo.png" />
-    <link rel="icon" href="/images/favicon.ico" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:title" content={title} />
 
-    <meta
-      property="og:description"
-      content={intl.format(metaData.description.body) as string}
-    />
-    <meta
-      property="twitter:description"
-      content={intl.format(metaData.description.body) as string}
-    />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/images/logo.png" />
+      <meta property="og:image:height" content="485" />
+      <meta property="og:image:width" content="772" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="/images/logo.png" />
 
-    <meta property="og:type" content="website" />
-    <meta name="twitter:card" content="summary_large_image" />
-    {/* FIXME: ここに Facebook の app_id */
-    /* <meta property="fb:app_id" content="" /> */
-    /* または adminID */
-    /* <meta property="fb:admins" content="" /> */}
-  </Head>
-);
+      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+    </Head>
+  );
+};
 FC.displayName = 'Helmet';
 
 export default FC;
